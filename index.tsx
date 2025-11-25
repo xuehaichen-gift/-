014fbbf2390c5ae792fc802755fc18067a5c2a1a
xuehaichen-gift <chenxuehai1991@gmail.com>
2025-11-25 21:28:32 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './src/index.css';  // 如果文件在 src 文件夹内
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
